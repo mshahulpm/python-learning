4a0034bbc43de56441fdf3d41591fe2aacf65a5a
Mohammed Shahul P.M <mshahulpm@gmail.com>
2025-04-23 21:38:46 +0530

    testing some pyton tools

---
+++ ruff/main.py
@@ -0,0 +1,6 @@
+from utils import print_name, some_util
+import os
+
+some = os.environ.get("")
+print_name("Shahul")
+some_util()
---
+++ ruff/utils.py
@@ -0,0 +1,6 @@
+def some_util():
+    pass
+
+
+def print_name(name: str):
+    print(name)
